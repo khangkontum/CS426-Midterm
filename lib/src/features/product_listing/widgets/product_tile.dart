1396efd51e7cfba3b0a0e9facc41002121cd0e1b
Nguyễn Hoàng Khang <50722745+khangkontum@users.noreply.github.com>
2022-07-04 16:25:41 +0700

    fix overflow listing product

--- lib/src/features/product_listing/widgets/product_tile.dart
+++ lib/src/features/product_listing/widgets/product_tile.dart
@@ -16,99 +16,95 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 10, bottom: 10, left: 20),
-      height: 150,
-      child: Flexible(
-        fit: FlexFit.loose,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-                width: 177,
-                child: Card(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8.0),
-                    child: GestureDetector(
-                      onTap: moreDetail,
-                      child: Image.network(
-                        product.imageLink.toString(),
-                        fit: BoxFit.fitWidth,
-                        loadingBuilder: (BuildContext context, Widget child,
-                            ImageChunkEvent? loadingProgress) {
-                          if (loadingProgress == null) {
-                            return child;
-                          }
-                          return Center(
-                            child: CircularProgressIndicator(
-                              value: loadingProgress.expectedTotalBytes != null
-                                  ? loadingProgress.cumulativeBytesLoaded /
-                                      loadingProgress.expectedTotalBytes!
-                                  : null,
-                            ),
-                          );
-                        },
-                        errorBuilder: (context, exception, stackTrace) =>
-                            Image.asset(
-                          'image/error_image.png',
-                          fit: BoxFit.fitHeight,
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width,
+      child: Container(
+        padding:
+            const EdgeInsets.only(top: 10, bottom: 10, left: 20, right: 20),
+        height: 150,
+        child: Flexible(
+          fit: FlexFit.loose,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                  width: size.width * .43,
+                  child: Card(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: GestureDetector(
+                        onTap: moreDetail,
+                        child: Image.network(
+                          product.imageLink.toString(),
+                          fit: BoxFit.fitWidth,
+                          loadingBuilder: (BuildContext context, Widget child,
+                              ImageChunkEvent? loadingProgress) {
+                            if (loadingProgress == null) {
+                              return child;
+                            }
+                            return Center(
+                              child: CircularProgressIndicator(
+                                value: loadingProgress.expectedTotalBytes !=
+                                        null
+                                    ? loadingProgress.cumulativeBytesLoaded /
+                                        loadingProgress.expectedTotalBytes!
+                                    : null,
+                              ),
+                            );
+                          },
+                          errorBuilder: (context, exception, stackTrace) =>
+                              Image.asset(
+                            'image/error_image.png',
+                            fit: BoxFit.fitHeight,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                )),
-            const SizedBox(width: 21),
-            Flexible(
-              fit: FlexFit.loose,
-              child: SizedBox(
-                width: 177,
+                  )),
+              const SizedBox(width: 21),
+              Flexible(
+                fit: FlexFit.loose,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: AutoSizeText(
-                        product.name.toString(),
-                        textAlign: TextAlign.left,
-                        style: Theme.of(context).textTheme.subtitle2,
-                        minFontSize: 18,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
+                    AutoSizeText(
+                      product.name.toString(),
+                      textAlign: TextAlign.left,
+                      style: Theme.of(context).textTheme.subtitle2,
+                      minFontSize: 18,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
                     ),
                     const SizedBox(height: 12),
-                    Align(
-                      alignment: Alignment.topLeft,
-                      child: Row(
-                        children: [
-                          AutoSizeText(
-                            product.price.toString(),
-                            textAlign: TextAlign.left,
-                            style: Theme.of(context).textTheme.subtitle2,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                          AutoSizeText(
-                            product.priceSign == Null
-                                ? product.priceSign.toString()
-                                : "\$",
-                            textAlign: TextAlign.left,
-                            style: Theme.of(context).textTheme.bodyText1,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ],
-                      ),
+                    Row(
+                      children: [
+                        AutoSizeText(
+                          product.price.toString(),
+                          textAlign: TextAlign.left,
+                          style: Theme.of(context).textTheme.subtitle2,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        AutoSizeText(
+                          product.priceSign == Null
+                              ? product.priceSign.toString()
+                              : "\$",
+                          textAlign: TextAlign.left,
+                          style: Theme.of(context).textTheme.bodyText1,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ],
                     ),
                     const SizedBox(height: 14),
                     Row(
                       children: [
-                        SizedBox(
-                          width: 177,
-                          height: 50,
-                          child: ElevatedButton(
-                            onPressed: () =>
-                                addToCart(itemFromProduct(product)),
+                        ElevatedButton(
+                          onPressed: () => addToCart(itemFromProduct(product)),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 0, vertical: 16),
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: const [
@@ -125,9 +121,9 @@ class ProductTile extends StatelessWidget {
                     )
                   ],
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
